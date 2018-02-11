5436a4bb139a25f0bdfca52c328355d886aecb3a
Nam Huynh <namhuynh156@gmail.com>
2018-02-12 02:19:34 +0700

    update hint

--- libs/spider-queen.js
+++ libs/spider-queen.js
@@ -111,7 +111,7 @@ const HINTS_LIST = [
     { crawler: 'caribbeancom', regex: /^(carib|caribbean) \d{6}-\d{3}$/ },
     { crawler: 'caribbeancompr', regex: /^(caribpr|caribbeanpr) \d{6}_\d{3}$/ },
     { crawler: 'tokyo-hot', regex: /^tokyohot .*$/ },
-    { crawler: 'avent', regex: /^(sky|red|rhj|skyhd|pt|pb|hey|bt)-\d{3}$/ },
+    { crawler: 'avent', regex: /^(sky|red|rhj|skyhd|pt|pb|hey|bt|drc|s2m)-\d{3}$/ },
     { crawler: 'avent', regex: /^(cwp|cwdv|cz|smd|laf|kg|dsam)-\d+$/ },
     { crawler: 'r18', regex: /.*/ }, // unblockdmm.com is current down 18/01/23
 ];
