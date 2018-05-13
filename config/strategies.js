23abf92aea477404c50a1813015a362bb3f2b853
Nam Huynh <namhuynh156@gmail.com>
2018-05-14 06:56:38 +0700

    add stragety

--- config/strategies.js
+++ config/strategies.js
@@ -10,7 +10,7 @@ module.exports = {
             { crawler: 'avent', condition: /^(sky|red|rhj|skyhd|pt|pb|hey|bt|drc|s2m|ssk-bd|sskj|trg|pink)-\d{3}$/ },
             { crawler: 'avent', condition: /^(cwp|cwdv|cz|smd|laf|kg|dsam|shd|smr)-\d+$/ },
             { crawler: 'avent', condition: /^(mkd-s|mkbd-s)\d+$/ },
-            { crawler: 'avent', condition: /^cld\d{2}}$/ },
+            { crawler: 'avent', condition: /^cld\d{2}$/ },
         ],
     
         all: [
