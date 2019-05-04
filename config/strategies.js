1c999de4464bbfc9d446099f12d7018e926ba83a
Nam Huỳnh <namhuynh156@gmail.com>
2019-05-04 13:59:21 +0700

    update stragety

--- config/strategies.js
+++ config/strategies.js
@@ -8,6 +8,7 @@ module.exports = {
             { crawler: 'tokyo-hot', condition: /^tokyohot .*$/ },
             { crawler: 'pacopacomama', condition: /^paco \d{6}_\d{3}$/ },
             { crawler: 'avent', condition: /^(emp|sky|red|rhj|skyhd|pt|pb|hey|bt|drc|s2m|ssk-bd|sskj|trg|trp|pink)-\d{3}$/ },
+            { crawler: 'avent', condition: /^bt-\d{2}$/ },
             { crawler: 'avent', condition: /^(cwp|cwdv|cz|smd|smbd|laf|kg|dsam|shd|smr)-\d+$/ },
             { crawler: 'avent', condition: /^(mkd-s|mkbd-s)\d+$/ },
             { crawler: 'avent', condition: /^cld\d{2}$/ },
